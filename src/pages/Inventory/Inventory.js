cc49ddaec0a7d08b5f3fcbf77ab5deadc9a95f73
arifibnenam <arifibnenam@gmail.com>
2022-04-30 13:05:08 +0600

    deliverd btn set

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,22 +1,47 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import useItem from '../../hooks/useItem';
+import Loading from '../Loading/Loding';
 import Item from '../Shared/Item/Item';
 
 const Inventory = () => {
     const { id } = useParams();
     const [item, setItem] = useState({});
+    const [modifiedCount, setModifiedCount] = useState(0);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         fetch(`http://localhost:5000/item/${id}`)
             .then(res => res.json())
             .then(data => setItem(data))
-    }, [])
+    }, [modifiedCount]);
+    const handleDelivered = () => {
+        setLoading(true);
+        fetch(`http://localhost:5000/item/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({})
+        })
+            .then(res => res.json())
+            .then(async data => {
+                const newCount = modifiedCount + data.modifiedCount;
+                await setModifiedCount(newCount);
+                setLoading(false);
+
+            })
+    }
     return (
-        <div className='d-flex justify-content-center mt-5 mx-3'>
-            <Item item={item}>
-                <button className='btn btn-primary'>Delivered</button>
-            </Item>
-        </div>
+        <>
+            {
+                loading ? <Loading></Loading>
+                    : <div className='d-flex justify-content-center mt-5 mx-3'>
+                        <Item item={item}>
+                            <button onClick={handleDelivered} className='btn btn-primary'>Delivered</button>
+                        </Item>
+                    </div>
+            }
+        </>
     );
 };
 
