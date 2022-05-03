efaf4b0d1764d5f511a2ea09b4134551aea7e677
arifibnenam <arifibnenam@gmail.com>
2022-05-03 06:02:48 +0600

    carousel added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyCarousel from '../MyCarousel/MyCarousel';
 import Items from '../Shared/Items/Items';
 
 const Home = () => {
     return (
         <div>
+            <MyCarousel></MyCarousel>
             <Items></Items>
         </div>
     );
---
+++ src/pages/MyCarousel/MyCarousel.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../images/banner/banner1-min.jpg';
+import banner2 from '../../images/banner/banner2-min.jpg';
+import banner3 from '../../images/banner/banner3-min.jpg';
+const MyCarousel = () => {
+    return (
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default MyCarousel;
